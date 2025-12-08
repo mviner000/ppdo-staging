95a0d2f79116c3c90a074833614925b11b6400f9
primex-melvin <nogoy.melvin@primex.ventures>
2025-12-09 05:21:57 +0800

    dashboard components added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["www.thiings.co", "images.unsplash.com", "c0.wallpaperflare.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.thiings.co",
+      },
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "c0.wallpaperflare.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
